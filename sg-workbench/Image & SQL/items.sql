09030fccb7471ad5d42b5653b00fb87bbd8d8520
SimpleGamerDK <65606088+SimpleGamerDK@users.noreply.github.com>
2022-08-10 19:06:15 +0200

    Delete sg-workbench directory

--- sg-workbench/Image & SQL/items.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`, `stackable`, `closeonuse`, `desc`) VALUES
-('toolbox', 'Tool Box', 14, 0, 1, 1, 1, 'A small portable tool box');
